577760534170eb9b76906f4403939080e14ce89e
franciscogaluppo <froma.galuppo@gmail.com>
2019-09-18 10:43:15 -0300

    Some vertices are featureless

--- utils/test_pubmed.py
+++ utils/test_pubmed.py
@@ -1,28 +1,34 @@
 import sys
 import numpy as np
-import _pikcle as cPickle
+import _pickle as cPickle
 
 sys.path.insert(0,'../')
 
 import model.tensorflow_model as model
 
 # Reading the data
-features = cPickle.load(open("datasets/pubmed/ind.pubmed.allx"))
-labels = cPickle.load(open("datasets/pubmed/ind.pubmed.ally"))
-graph = cPickle.load(open("datasets/pubmed/ind.pubmed.graph"))
+features = cPickle.load(open("../datasets/pubmed/ind.pubmed.allx", 'rb'),encoding="latin1").toarray()
+labels = cPickle.load(open("../datasets/pubmed/ind.pubmed.ally", 'rb'))
+graph = cPickle.load(open("../datasets/pubmed/ind.pubmed.graph", 'rb'))
 
-n = labels.shape[0]
+# Creating adjacency matrix
+n = len(graph)
 adj = [[0]*n]*n
 
 for i in graph:
     for j in graph[i]:
+        print(i, j)
         adj[i][j] = 1
+
+print(len(features))
+
+quit()
 
 #-------------------------------------------------------------#
 
 method = 2
 n_lab, n_train, n_unlab = .2, .6, .2
-G = sparse(adj, attr, labels)
+G = model(adj, attr, labels)
 num_epochs, lr = 10, 0.5
 
 #-------------------------------------------------------------#
